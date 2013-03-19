412a0a588a077e381212ea2ea126a75f56b7a494
Yuliya Bozhko <yuliya.bozhko@totaralms.com>
2013-03-20 11:53:49 +1300

    Fix assertion

--- badges/backpack.js
+++ badges/backpack.js
@@ -2,5 +2,5 @@
  * Push badges to backpack.
  */
 function addtobackpack(event, args) {
-    OpenBadges.issue(['\'' + args.assertion + '\''], function(errors, successes) { });
+    OpenBadges.issue([args.assertion], function(errors, successes) { });
 }
